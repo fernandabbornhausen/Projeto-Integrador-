4ce3dac4a843145a077d24aadbb1d74b92307ae3
leonardommva <leonardo.mmva@gmail.com>
2024-08-30 21:15:26 -0300

    Correção da barra de pesquisa

--- Java/eventosefeitas.js
+++ Java/eventosefeitas.js
@@ -1,23 +1,25 @@
 document.getElementById('search-button').addEventListener('click', function() {
-    const procurar = document.getElementById('buscar').value.toLowerCase();
+    const procurar = document.getElementById('buscar').value.toLowerCase()
+        .normalize('NFD').replace(/[\u0300-\u036f]/g, '');  
     const eventos = document.querySelectorAll('.evento');
     let found = false;
 
     eventos.forEach(cidade => {
-        const cidadeNome = cidade.getAttribute('data-city').toLowerCase();
+        const cidadeNome = cidade.getAttribute('data-city').toLowerCase()
+            .normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
         if (cidadeNome.includes(procurar)) {
-            cidade.classList.remove('hidden'); // Mostra a cidade correspondente
+            cidade.classList.remove('hidden');
             found = true;
         } else {
-            cidade.classList.add('hidden'); // Esconde a cidade que não corresponde
+            cidade.classList.add('hidden');
         }
     });
 
     const noResults = document.getElementById('no-results');
     if (!found && procurar !== '') {
-        noResults.style.display = 'block'; // Mostra a mensagem de "Nenhum resultado"
+        noResults.style.display = 'block'; 
     } else {
-        noResults.style.display = 'none'; // Esconde a mensagem de "Nenhum resultado"
+        noResults.style.display = 'none';
     }
 });
